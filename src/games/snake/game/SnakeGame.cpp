f9524d0537ebb01c1ec0b50b84c4f689979f80bb
sdragos1 <dragossuceveanu@yahoo.com>
2024-03-25 14:19:25 +0100

    feat(games): add snake game provider

---
+++ src/games/snake/game/SnakeGame.cpp
@@ -0,0 +1,51 @@
+/*
+** EPITECH PROJECT, 2024
+** arcade [WSL: Ubuntu-22.04]
+** File description:
+** Snake
+*/
+
+#include "SnakeGame.hpp"
+
+SnakeGame::SnakeGame()
+    :   _entities()
+{
+    SnakeHeadEntity head;
+    _entities[head.getId()] = std::make_shared<SnakeHeadEntity>(head);
+}
+
+SnakeGame::~SnakeGame()
+{
+}
+
+void SnakeGame::compute(DeltaTime dt)
+{
+    (void)dt;
+    std::cout << "bozo" << std::endl;
+}
+
+const GameManifest &SnakeGame::getManifest(void) const noexcept
+{
+    return snakeManifest;
+}
+
+const Vector2u SnakeGame::getWindowMinSize(void) const noexcept
+{
+    return {20, 20};
+}
+
+
+const Vector2u SnakeGame::getSize(void) const noexcept
+{
+    return {20, 20};
+}
+
+const entity::EntitiesMap &SnakeGame::getEntities(void) const
+{
+    return _entities;
+}
+
+std::shared_ptr<entity::IEntity> SnakeGame::getEntityById(const UUId &id) const
+{
+    return _entities.at(id);
+}
