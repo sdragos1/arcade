cb66f0e7eab6d6395725bc92af60adbf08d2b0ee
Baptiste Moreau <baptiste.moreau@epitech.eu>
2024-03-28 15:07:32 +0100

    #15 feat: advancement on the snake's head implementation

---
+++ src/games/snake/entities/snake_head/components/SnakeHeadKeyboard.cpp
@@ -0,0 +1,59 @@
+/*
+** EPITECH PROJECT, 2024
+** arcade [WSL: Ubuntu-22.04]
+** File description:
+** SnakeHeadKeyboard
+*/
+
+#include "SnakeHeadKeyboard.hpp"
+
+SnakeHeadKeyboard::SnakeHeadKeyboard(const entity::IEntity &entity)
+    :
+    _entity(entity)
+{
+}
+
+SnakeHeadKeyboard::~SnakeHeadKeyboard()
+{
+}
+
+const components::ComponentType SnakeHeadKeyboard::getType() const noexcept
+{
+    return components::KEYBOARD;
+}
+
+const entity::IEntity &SnakeHeadKeyboard::getEntity() noexcept
+{
+    return _entity;
+}
+
+void SnakeHeadKeyboard::onKeyPress(std::shared_ptr<IGame> &ctx, components::KeyData key)
+{
+    (void)ctx;
+    std::shared_ptr<SnakeHeadDisplayable> displayable = nullptr;
+    for (auto &component : _entity.getComponents()) {
+        if (component->getType() == components::ComponentType::TEXTURE) {
+            displayable = std::dynamic_pointer_cast<SnakeHeadDisplayable>(component);
+            break;
+        }
+    }
+    if (displayable == nullptr)
+        return;
+    if (key.type != components::KeyType::ARROW)
+        return;
+    if (key.code.arrow == components::ArrowCode::UP) {
+        displayable->_position.y -= 1;
+    } else if (key.code.arrow == components::ArrowCode::DOWN) {
+        displayable->_position.y += 1;
+    } else if (key.code.arrow == components::ArrowCode::LEFT) {
+        displayable->_position.x -= 1;
+    } else if (key.code.arrow == components::ArrowCode::RIGHT) {
+        displayable->_position.x += 1;
+    }
+}
+
+void SnakeHeadKeyboard::onKeyRelease(std::shared_ptr<IGame> &ctx, components::KeyData key)
+{
+    (void)ctx;
+    (void)key;
+}
---
+++ src/games/snake/entities/snake_head/components/SnakeHeadKeyboard.hpp
@@ -0,0 +1,28 @@
+/*
+** EPITECH PROJECT, 2024
+** arcade [WSL: Ubuntu-22.04]
+** File description:
+** SnakeHeadKeyboard
+*/
+
+#pragma once
+
+#include "games/components/IKeyboardComponent.hpp"
+#include "games/components/IDisplayableComponent.hpp"
+#include "SnakeHeadDisplayable.hpp"
+
+using namespace shared::games;
+
+class SnakeHeadKeyboard : public components::IKeyboardComponent {
+    public:
+        explicit SnakeHeadKeyboard(const entity::IEntity &entity);
+        ~SnakeHeadKeyboard();
+
+        const components::ComponentType getType() const noexcept override;
+        const entity::IEntity &getEntity() noexcept override;
+
+        void onKeyPress(std::shared_ptr<IGame> &ctx, components::KeyData key) override;
+        void onKeyRelease(std::shared_ptr<IGame> &ctx, components::KeyData key) override;
+    private:
+        const entity::IEntity &_entity;
+};
