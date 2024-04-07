23d72c7f0d9296dd3e2381e4fdd223f094c6af09
sdragos1 <dragossuceveanu@yahoo.com>
2024-04-07 19:40:47 +0200

    rebase 2

--- src/games/snake/entities/snake_tail/SnakeTailEntity.hpp
+++ src/games/snake/entities/snake_tail/SnakeTailEntity.hpp
@@ -8,13 +8,18 @@
 #pragma once
 
 #include "components/SnakeTailDisplayable.hpp"
+<<<<<<< HEAD
 #include "components/SnakeTailCollidable.hpp"
+=======
+#include "components/SnakeTailKeyboard.hpp"
+>>>>>>> 0c77c95 (#15 feat: advancement on the snake tail implementation)
 #include "games/IEntity.hpp"
 
 using namespace shared::games;
 
 class SnakeTailEntity : public entity::IEntity {
     public:
+<<<<<<< HEAD
         /**
          * @brief Construct a new Snake Tail Entity object
          */
@@ -29,8 +34,17 @@ class SnakeTailEntity : public entity::IEntity {
          * @brief Get the Components object
          * @return const components::ComponentsMap&
          */
+=======
+        SnakeTailEntity();
+        ~SnakeTailEntity();
+
+>>>>>>> 0c77c95 (#15 feat: advancement on the snake tail implementation)
         const components::ComponentsMap &getComponents(void) const noexcept override;
 
     private:
         components::ComponentsMap _components;
+<<<<<<< HEAD
 };
+=======
+};
+>>>>>>> 0c77c95 (#15 feat: advancement on the snake tail implementation)
--- src/games/snake/entities/snake_tail/components/SnakeTailDisplayable.hpp
+++ src/games/snake/entities/snake_tail/components/SnakeTailDisplayable.hpp
@@ -13,6 +13,7 @@ using namespace shared::games;
 
 class SnakeTailDisplayable : public components::ITextureComponent {
     public:
+<<<<<<< HEAD
         /**
          * @brief Construct a new Snake Tail Displayable object
          * @param entity
@@ -104,9 +105,31 @@ class SnakeTailDisplayable : public components::ITextureComponent {
         Vector2f _position;
         Vector2f _oldPosition;
         components::TextureProps _textureProps;
+=======
+        explicit SnakeTailDisplayable(const entity::IEntity &entity);
+        ~SnakeTailDisplayable();
+
+        const components::ComponentType getType() const noexcept override;
+        const entity::IEntity &getEntity() noexcept override;
+
+        Vector2u &getSize(void) noexcept override;
+        unsigned int &getZIndex(void) noexcept override;
+        components::TextureProps &getTextureProps(void) noexcept override;
+        void onMousePress(std::shared_ptr<IGame> &ctx) override;
+        void onMouseHover(std::shared_ptr<IGame> &ctx) override;
+        void onMouseRelease(std::shared_ptr<IGame> &ctx) override;
+
+        Vector2i &getPosition(void) noexcept override;
+        Vector2i _position;
+>>>>>>> 0c77c95 (#15 feat: advancement on the snake tail implementation)
 
     private:
         const entity::IEntity &_entity;
         Vector2u _size;
         unsigned int _zIndex;
+<<<<<<< HEAD
 };
+=======
+        components::TextureProps _textureProps;
+};
+>>>>>>> 0c77c95 (#15 feat: advancement on the snake tail implementation)
